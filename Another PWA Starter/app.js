7bcaf6c0292ed0df9379a7cf31796db250c98651
Elise Perry-Hayek <emh5943@rit.edu>
2024-10-18 11:24:11 -0400

    10/18

--- Another PWA Starter/app.js
+++ Another PWA Starter/app.js
@@ -40,3 +40,115 @@ request.onupgradeneeded = function (event) {
     }
   );
 };
+
+  //create any new object stores for our db or delete any old ones from a previous version
+  const objectStore = db.createObjectStore("jokeData", 
+    {
+      keyPath: "id",
+  });
+
+  //create a broadcast channel - name here needs to match the name in the sw
+const channel = new BroadcastChannel("jokes_channel");
+
+//listen for messages
+channel.onmessage = (event) => {
+  console.log("Received a message in PWA: ", event.data);
+  
+  if (event.data === "data-updated") {
+    //update the UI or perform other actions
+    console.log("Data Updated!");
+    renderPastJokes();
+  }
+};
+
+//refresh the UI
+renderPastJokes();
+
+//make sure the DOM is fully loaded before trying to access it
+document.addEventListener("DOMContentLoaded",()=>{
+  const sendButton = document.getElementById("sendButton");
+
+  if (sendButton) {
+    sendButton.addEventListener("click", requestJokes);
+  } else {
+    console.error("Button with ID 'sendButton' not found");
+  }
+});
+
+function getAllJokes() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(dbName);
+
+    request.onerror = (event) => {
+      reject(`Database error: ${event.target.error}`);
+    };
+    
+    request.onsuccess = (event) => {
+      const db = event.target.result;
+      const transaction = db.transaction("jokeData", "readonly");
+      const objectStore = transaction.objectStore("jokeData");
+
+      const objects = [];
+      objectStore.openCursor().onsuccess = (event) => {
+        const cursor = event.target.result;
+
+        if (cursor) {
+          //add each object to our array
+          objects.push(cursor.value);
+          cursor.continue();
+        } else {
+          //no more objects to iterate, resolve the promise
+          resolve(objects);
+        }
+      };
+      transaction.oncomplete = () => {
+        db.close();
+      };
+    };
+  });
+} //getAllJokes
+
+function renderPastJokes() {
+
+  getAllJokes()
+    .then((jokes)=>{
+
+      console.log("All objects: ", jokes);
+      if (jokes.length > 0) {
+        const jokeList = document.getElementById("allJokes");
+        //clear the list of past jokes since we're going to re-render it
+        jokeList.innerHTML = "";
+
+        const pastJokesHeader = document.createElement("h2");
+        pastJokesHeader.textContent = "Saved Jokes";
+
+        const pastJokesList = document.createElement("ul");
+        pastJokesList.id = "jokeList";
+
+        //loop over each joke and render it
+        jokes.forEach((joke)=>{
+          const jokeEl = document.createElement("li");
+          jokeEl.innerHTML = `<div>
+            <span class="title">Setup: </span>${joke.setup}<br />
+            <span class="title">Delivery: </span>${joke.delivery}
+          </div>`;
+
+          const deleteButton = document.createElement("button");
+          deleteButton.textContent = "Delete";
+          deleteButton.classList.add("delete-btn");
+          deleteButton.addEventListener("click", ()=>{
+            deleteJoke(joke.id);
+          });
+          jokeEl.appendChild(deleteButton);
+          pastJokesList.appendChild(jokeEl);
+        });
+        jokeList.appendChild(pastJokesHeader);
+        jokeList.appendChild(pastJokesList);
+      }
+
+    })
+    .catch((error)=>{
+      console.error("Error retrieving objects: ", error);
+    })
+
+}//renderPastJokes
